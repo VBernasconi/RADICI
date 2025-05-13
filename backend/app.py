5143223a1fa2e2efabcc75fdd59881d479475b8b
Valentine Bernasconi <valentineb@bluewin.ch>
2025-05-13 14:31:34 +0200

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
 # app.py
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from huggingface_hub import hf_hub_download
 import pandas as pd
 import numpy as np
 import ast
@@ -12,74 +9,30 @@ import faiss
 from image_search import build_faiss_index, load_embeddings, find_similar_images
 
 
-#app = Flask(__name__)
-app = FastAPI()
-#CORS(app)  # allow frontend calls
-# CORS config
-app.add_middleware(
-                   CORSMiddleware,
-                   allow_origins=["*"],  # Adjust this in production
-                   allow_credentials=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"],
-                   )
+app = Flask(__name__)
+CORS(app)  # allow frontend calls
 
-INDEX_FILE = "index.faiss"
-CSV_FILE = "dataset_with_embeddings.csv"
-EMBEDDINGS_FILE = "dataset_with_embeddings.pkl" #"dataset_with_parsed_embeddings.csv"
-
-REPO_ID = "VBernasconi/RADICI"
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-# Download CSV file
-CSV_PATH = hf_hub_download(
-                           repo_id = REPO_ID,
-                           filename = CSV_FILE,
-                           repo_type = "dataset",
-                           token = HF_TOKEN
-                           )
-
-# Download PICKLE file
-EMBEDDINGS_PATH = hf_hub_download(
-                                  repo_id = REPO_ID,
-                                  filename = EMBEDDINGS_FILE,
-                                  repo_type = "dataset",
-                                  token = HF_TOKEN
-                                  )
-
-# Download the FAISS index
-INDEX_PATH = hf_hub_download(
-                             repo_id = REPO_ID,
-                             filename = INDEX_FILE,
-                             repo_type = "dataset",
-                             token = HF_TOKEN  # required for private repo
-                             )
+INDEX_PATH = "index.faiss"
+CSV_PATH = "dataset_with_embeddings.csv"
+EMBEDDINGS_PATH = "dataset_with_embeddings.pkl" #"dataset_with_parsed_embeddings.csv"
 
 # Check if index exists
-#if os.path.exists(INDEX_PATH):
-#    print("Loading existing FAISS index and metadata...")
+if os.path.exists(INDEX_PATH):
+    print("Loading existing FAISS index and metadata...")
     #df = pd.read_csv(EMBEDDINGS_PATH)
-#    df = pd.read_pickle(EMBEDDINGS_PATH)
+    df = pd.read_pickle(EMBEDDINGS_PATH)
+
     #df['parsed_embedding'] = df['parsed_embedding'].apply(lambda x: np.array(x, dtype=np.float32))
-#    index = faiss.read_index(INDEX_PATH)
-#else:
-#    print("Building FAISS index...")
-#    df, embedding_matrix = load_embeddings(CSV_PATH)
-#    df.to_pickle(EMBEDDINGS_PATH)
+    index = faiss.read_index(INDEX_PATH)
+else:
+    print("Building FAISS index...")
+    df, embedding_matrix = load_embeddings(CSV_PATH)
+    df.to_pickle(EMBEDDINGS_PATH)
     #df['parsed_embedding'] = df['parsed_embedding'].apply(lambda x: str(list(x)))
     #df.to_csv(EMBEDDINGS_PATH, index=False)
     # Build the index
-#    index = build_faiss_index(embedding_matrix)
-#    faiss.write_index(index, INDEX_PATH)
-
-
-# Load df
-# df, embedding_matrix = load_embeddings(CSV_PATH)
-
-# Load embeddings
-df = pd.read_pickle(EMBEDDINGS_PATH)
-# Load into FAISS
-index = faiss.read_index(index_path)
+    index = build_faiss_index(embedding_matrix)
+    faiss.write_index(index, INDEX_PATH)
 
 
 @app.route('/images/<image_file>')
